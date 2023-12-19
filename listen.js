14495c6bdcd69fdb734fe6e376e4c76c9b31b77f
Nadine Poisson <nadinepoisson000@gmail.com>
2023-12-19 14:34:05 +0100

    maj ex 9

--- listen.js
+++ listen.js
@@ -1,1 +1,13 @@
 // CODE HERE
+let p = document.querySelector("p");
+function tapeBtnOn() {
+ p.classList.toggle('hidden')
+}
+let button = document.querySelector("button");
+button.addEventListener("click", tapeBtnOn);
+
+
+
+
+
+
